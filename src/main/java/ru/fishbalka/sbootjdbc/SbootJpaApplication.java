c46aeb701e5e1c65f524c132a6d1493c78f617da
Sergey Mokhov <s.v.mokhov@gmail.com>
2019-03-01 08:10:02 +0300

    initial

---
+++ src/main/java/ru/fishbalka/sbootjdbc/SbootJpaApplication.java
@@ -0,0 +1,13 @@
+package ru.fishbalka.sbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbootJpaApplication.class, args);
+    }
+
+}
